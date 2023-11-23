e0c960c9768530d8a75f09604f0759555ebbc861
Sumit <sumitjoshi181@gmail.com>
2023-11-23 22:12:17 +0530

    image upload commit 2

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -13,13 +13,17 @@ exports.upload = multer({ storage: storage, limits: { fileSize: 20971520 } }); /
 exports.uploadMultiple = exports.upload.array("files", 10);
 
 exports.uploadSingleFile = async (req, res) => {
-  const file = req.file;
-  if (!file) {
+  try {
+    const file = req.file;
+    if (!file) throw new Error("Please upload a file.");
+
+    res.send({ error: false, status: 200, file });
+  } catch (error) {
+    console.log("🚀 ~ file: upload.controller.js:22 ~ exports.uploadSingleFile= ~ error:", error)
     return res.status(200).json({
       error: true,
       status: 400,
-      message: "Please upload a file.",
+      message: error.message,
     });
   }
-  res.send({ error: false, status: 200, file });
 };
