11cc40ec4b016b9976d3b69bedf73f87b8242e1e
Sumit <sumitjoshi181@gmail.com>
2023-12-03 18:37:34 +0530

    update on post

--- server/utils/common.functions.js
+++ server/utils/common.functions.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 const formatNumber = (num) => {
   if (num < 1000) {
     return num.toString();
@@ -8,6 +10,26 @@ const formatNumber = (num) => {
   }
 };
 
+const getTimeDifferenceText = (timestamp) => {
+  const currentTime = Date.now(); // Get the current timestamp in milliseconds
+  const differenceInMilliseconds = currentTime - timestamp;
+
+  const minutesAgo = Math.floor(differenceInMilliseconds / (1000 * 60));
+  const hoursAgo = Math.floor(differenceInMilliseconds / (1000 * 60 * 60));
+  const daysAgo = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo} ${minutesAgo === 1 ? "minute" : "minutes"} ago`;
+  } else if (hoursAgo < 24) {
+    return `${hoursAgo} ${hoursAgo === 1 ? "hour" : "hours"} ago`;
+  } else if (minutesAgo < 10080) {
+    return `${daysAgo} ${daysAgo === 1 ? "day" : "days"} ago`;
+  } else {
+    return moment(timestamp).format("ll");
+  }
+};
+
 module.exports = {
   formatNumber,
+  getTimeDifferenceText,
 };
