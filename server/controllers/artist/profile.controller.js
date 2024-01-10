bf88ac453ca78c1e76f80e41bd45c713d98e9975
Sumit <sumitjoshi181@gmail.com>
2024-01-10 21:31:03 +0530

    login step 3

--- server/controllers/artist/profile.controller.js
+++ server/controllers/artist/profile.controller.js
@@ -1,12 +1,29 @@
+const User = require("../../models/User");
+
 /**
- * Customer Login : loginStep2 : verify OTP
+ *  Profile : Basic Info
  * Type : POST
- * Route : /artist/login/verify-code
+ * Route : /artist/profile/basic-info
  */
 exports.saveBasicInfo = async (req, res) => {
   try {
     const { _id } = req.user;
     const { email, first_name, last_name } = req.body;
+
+    //checking is email is already used
+    let user = await User.findOne({ email, _id: { $ne: _id } });
+
+    if (user) throw new Error("Email already in use.");
+
+    User.findByIdAndUpdate(_id, {
+      $set: {
+        email,
+        first_name,
+        last_name,
+      },
+    }).then(() => {
+      res.send({ error: false, message: "User updated" });
+    });
   } catch (error) {
     res.send({ error: true, message: error.message });
   }
